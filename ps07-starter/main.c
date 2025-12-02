96f12c2d58d610543f43926d4be6269b1651b8cd
sarahKlocki <sklocki@ucsc.edu>
2025-12-02 00:12:46 -0800

    GOATED. LOCK THE FUCK IN

---
+++ ps07-starter/main.c
@@ -0,0 +1,14 @@
+#include "string2.h"
+#include <stdio.h>
+
+int main(void) {
+    /* ───────── strlen checks ───────── */
+
+    /* ───────── strstr checks ───────── */
+
+    /* ───────── strncpy checks ───────── */
+
+    /* ───────── strcmp checks ───────── */
+
+    return 0;
+}
---
+++ ps07-starter/string2.h
@@ -0,0 +1,8 @@
+/* DO NOT MODIFY THIS FILE */
+
+#ifndef STRING2_H
+#define STRING2_H
+
+#include <string.h>
+
+#endif
